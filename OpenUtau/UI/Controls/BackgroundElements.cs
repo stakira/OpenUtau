b644b9d994cd4a6d804133d1c278c8acbdf7ad20
Sugita Akira <akira3weet@gmail.com>
2021-01-21 22:33:16 -0800

    Fix black white keys.

--- OpenUtau/UI/Controls/BackgroundElements.cs
+++ OpenUtau/UI/Controls/BackgroundElements.cs
@@ -10,15 +10,12 @@ using OpenUtau.UI.Models;
 using OpenUtau.Core;
 using OpenUtau.Core.USTx;
 
-namespace OpenUtau.UI.Controls
-{
-    class BackgroundElement : FrameworkElement
-    {
+namespace OpenUtau.UI.Controls {
+    class BackgroundElement : FrameworkElement {
         protected Size _size;
         protected bool _updated = false;
 
-        public BackgroundElement()
-        {
+        public BackgroundElement() {
             this.SizeChanged += (o, e) => { _size = e.NewSize; MarkUpdate(); };
         }
 
@@ -29,16 +26,13 @@ namespace OpenUtau.UI.Controls
         public static void MarkUpdateCallback(DependencyObject source, DependencyPropertyChangedEventArgs e) { ((BackgroundElement)source).MarkUpdate(); }
     }
 
-    class TrackBackground : BackgroundElement
-    {
-        public double TrackHeight
-        {
+    class TrackBackground : BackgroundElement {
+        public double TrackHeight {
             set { SetValue(TrackHeightProperty, value); }
             get { return (double)GetValue(TrackHeightProperty); }
         }
 
-        public double OffsetY
-        {
+        public double OffsetY {
             set { SetValue(OffsetYProperty, value); }
             get { return (double)GetValue(OffsetYProperty); }
         }
@@ -46,21 +40,18 @@ namespace OpenUtau.UI.Controls
         public static readonly DependencyProperty TrackHeightProperty = DependencyProperty.Register("TrackHeight", typeof(double), typeof(TrackBackground), new PropertyMetadata(0.0, MarkUpdateCallback));
         public static readonly DependencyProperty OffsetYProperty = DependencyProperty.Register("OffsetY", typeof(double), typeof(TrackBackground), new PropertyMetadata(0.0, MarkUpdateCallback));
 
-        public TrackBackground()
-        {
+        public TrackBackground() {
             this.VerticalAlignment = VerticalAlignment.Stretch;
             this.HorizontalAlignment = HorizontalAlignment.Stretch;
             this.VisualEdgeMode = EdgeMode.Aliased;
         }
 
-        protected override void OnRender(DrawingContext drawingContext)
-        {
+        protected override void OnRender(DrawingContext drawingContext) {
             int firstTrack = (int)(OffsetY / TrackHeight);
             bool alt = firstTrack % 2 == 1;
             double top = TrackHeight * firstTrack - OffsetY;
 
-            while (top < _size.Height)
-            {
+            while (top < _size.Height) {
                 drawingContext.DrawRectangle(
                     alt ? ThemeManager.TrackBackgroundBrushAlt : ThemeManager.TrackBackgroundBrush,
                     null,
@@ -71,101 +62,83 @@ namespace OpenUtau.UI.Controls
         }
     }
 
-    class KeyTrackBackground : TrackBackground
-    {
-        protected override void OnRender(DrawingContext drawingContext)
-        {
-            int firstTrack = (int)(OffsetY / TrackHeight);
-            int alt = firstTrack % 12;
-            double top = TrackHeight * firstTrack - OffsetY;
-
-            while (top < _size.Height)
-            {
+    class KeyTrackBackground : TrackBackground {
+        protected override void OnRender(DrawingContext drawingContext) {
+            int note = UIConstants.MaxNoteNum - 1 - (int)(OffsetY / TrackHeight);
+            double top = TrackHeight * (UIConstants.MaxNoteNum - note - 1) - OffsetY;
+            while (top < _size.Height) {
                 drawingContext.DrawRectangle(
-                    MusicMath.IsBlackKey(alt) ? ThemeManager.TrackBackgroundBrushAlt : ThemeManager.TrackBackgroundBrush,
+                    MusicMath.IsBlackKey(note) ? ThemeManager.TrackBackgroundBrushAlt : ThemeManager.TrackBackgroundBrush,
                     null,
                     new Rect(0, (int)top, _size.Width, TrackHeight));
                 top += TrackHeight;
-                alt = (alt + 1) % 12;
+                note--;
             }
         }
     }
 
-    class KeyboardBackground : KeyTrackBackground
-    {
-        protected override void OnRender(DrawingContext drawingContext)
-        {
-            int firstTrack = (int)(OffsetY / TrackHeight);
-            int alt = firstTrack;
-            double top = TrackHeight * firstTrack - OffsetY;
-
-            while (top < _size.Height)
-            {
+    class KeyboardBackground : KeyTrackBackground {
+        protected override void OnRender(DrawingContext drawingContext) {
+            int note = UIConstants.MaxNoteNum - 1 - (int)(OffsetY / TrackHeight);
+            double top = TrackHeight * (UIConstants.MaxNoteNum - note - 1) - OffsetY;
+            while (top < _size.Height) {
                 drawingContext.DrawRectangle(
-                    MusicMath.IsBlackKey(alt) ? ThemeManager.BlackKeyBrushNormal : 
-                    MusicMath.IsCenterKey(alt) ? ThemeManager.CenterKeyBrushNormal : ThemeManager.WhiteKeyBrushNormal,
+                    MusicMath.IsBlackKey(note) ? ThemeManager.BlackKeyBrushNormal :
+                    MusicMath.IsCenterKey(note) ? ThemeManager.CenterKeyBrushNormal : ThemeManager.WhiteKeyBrushNormal,
                     null,
                     new Rect(0, (int)top, _size.Width, TrackHeight));
 
-                if (TrackHeight >= 12)
-                {
+                if (TrackHeight >= 12) {
                     FormattedText text = new FormattedText(
-                        MusicMath.GetNoteString(UIConstants.MaxNoteNum - alt - 1),
+                        MusicMath.GetNoteString(note),
                         System.Threading.Thread.CurrentThread.CurrentUICulture,
                         FlowDirection.LeftToRight,
                         SystemFonts.CaptionFontFamily.GetTypefaces().First(),
                         12,
-                        MusicMath.IsBlackKey(alt) ? ThemeManager.BlackKeyNameBrushNormal :
-                        MusicMath.IsCenterKey(alt) ? ThemeManager.CenterKeyNameBrushNormal : ThemeManager.WhiteKeyNameBrushNormal
+                        MusicMath.IsBlackKey(note) ? ThemeManager.BlackKeyNameBrushNormal :
+                        MusicMath.IsCenterKey(note) ? ThemeManager.CenterKeyNameBrushNormal : ThemeManager.WhiteKeyNameBrushNormal,
+                        VisualTreeHelper.GetDpi(this).PixelsPerDip
                     );
                     drawingContext.DrawText(text, new Point(42 - text.Width, (int)(top + (TrackHeight - text.Height) / 2)));
                 }
                 top += TrackHeight;
-                alt ++;
+                note--;
             }
         }
     }
 
-    class TickBackground : BackgroundElement
-    {
-        public double QuarterWidth
-        {
+    class TickBackground : BackgroundElement {
+        public double QuarterWidth {
             set { SetValue(QuarterWidthProperty, value); }
             get { return (double)GetValue(QuarterWidthProperty); }
         }
 
-        public double MinTickWidth
-        {
+        public double MinTickWidth {
             set { SetValue(MinTickWidthProperty, value); }
             get { return (double)GetValue(MinTickWidthProperty); }
         }
 
-        public double OffsetX
-        {
+        public double OffsetX {
             set { SetValue(OffsetXProperty, value); }
             get { return (double)GetValue(OffsetXProperty); }
         }
 
-        public double QuarterOffset
-        {
+        public double QuarterOffset {
             set { SetValue(QuarterOffsetProperty, value); }
             get { return (double)GetValue(QuarterOffsetProperty); }
         }
 
-        public int BeatPerBar 
-        {
+        public int BeatPerBar {
             set { SetValue(BeatPerBarProperty, value); }
             get { return (int)GetValue(BeatPerBarProperty); }
         }
 
-        public int BeatUnit
-        {
+        public int BeatUnit {
             set { SetValue(BeatUnitProperty, value); }
             get { return (int)GetValue(BeatUnitProperty); }
         }
 
-        public int TickMode
-        {
+        public int TickMode {
             set { SetValue(TickModeProperty, value); }
             get { return (int)GetValue(TickModeProperty); }
         }
@@ -180,8 +153,7 @@ namespace OpenUtau.UI.Controls
 
         protected Pen darkPen, lightPen, dashedPen;
 
-        public TickBackground()
-        {
+        public TickBackground() {
             this.VerticalAlignment = VerticalAlignment.Stretch;
             this.HorizontalAlignment = HorizontalAlignment.Stretch;
             this.VisualEdgeMode = EdgeMode.Aliased;
@@ -190,36 +162,27 @@ namespace OpenUtau.UI.Controls
             dashedPen = new Pen(ThemeManager.TickLineBrushLight, 1) { DashStyle = new DashStyle(UIConstants.DashLineArray, 0) };
         }
 
-        protected override void OnRender(DrawingContext drawingContext)
-        {
+        protected override void OnRender(DrawingContext drawingContext) {
             double zoomRatio = MusicMath.getZoomRatio(QuarterWidth, BeatPerBar, BeatUnit, MinTickWidth);
             double interval = zoomRatio * QuarterWidth;
             int tick = (int)((OffsetX + QuarterOffset * QuarterWidth) / interval) + 1;
             double left = tick * interval - OffsetX - QuarterOffset * QuarterWidth;
 
-            while (left < _size.Width)
-            {
+            while (left < _size.Width) {
                 double snappedLeft = Math.Round(left) + 0.5;
-                if ((tick * zoomRatio * BeatUnit) % (BeatPerBar * 4) == 0)
-                {
+                if ((tick * zoomRatio * BeatUnit) % (BeatPerBar * 4) == 0) {
                     drawingContext.DrawLine(darkPen, new Point(snappedLeft, -0.5), new Point(snappedLeft, ActualHeight + 0.5));
-                }
-                else if ((tick * zoomRatio * BeatUnit) % 4 == 0)
-                {
+                } else if ((tick * zoomRatio * BeatUnit) % 4 == 0) {
                     if (TickMode == 1)
                         drawingContext.DrawLine(darkPen, new Point(snappedLeft, -0.5), new Point(snappedLeft, ActualHeight + 0.5));
                     else
                         drawingContext.DrawLine(lightPen, new Point(snappedLeft, -0.5), new Point(snappedLeft, ActualHeight + 0.5));
-                }
-                else if ((tick * zoomRatio * BeatUnit) % 1 == 0)
-                {
+                } else if ((tick * zoomRatio * BeatUnit) % 1 == 0) {
                     if (TickMode == 1)
                         drawingContext.DrawLine(lightPen, new Point(snappedLeft, -0.5), new Point(snappedLeft, ActualHeight + 0.5));
                     else
                         drawingContext.DrawLine(dashedPen, new Point(snappedLeft, -0.5), new Point(snappedLeft, ActualHeight + 0.5));
-                }
-                else
-                {
+                } else {
                     drawingContext.DrawLine(dashedPen, new Point(snappedLeft, -0.5), new Point(snappedLeft, ActualHeight + 0.5));
                 }
                 left += interval;
@@ -228,38 +191,33 @@ namespace OpenUtau.UI.Controls
         }
     }
 
-    class TimelineBackground : TickBackground
-    {
+    class TimelineBackground : TickBackground {
         Dictionary<int, FormattedText> fTextPool = new Dictionary<int, FormattedText>();
 
-        protected override void OnRender(DrawingContext drawingContext)
-        {
+        protected override void OnRender(DrawingContext drawingContext) {
             double zoomRatio = MusicMath.getZoomRatio(QuarterWidth, BeatPerBar, BeatUnit, MinTickWidth);
             double interval = zoomRatio * QuarterWidth;
             int tick = (int)((OffsetX + QuarterOffset * QuarterWidth) / interval);
             double left = tick * interval - OffsetX - QuarterOffset * QuarterWidth;
             bool first_tick = true;
 
-            while (left < _size.Width)
-            {
+            while (left < _size.Width) {
                 double snappedLeft = Math.Round(left) + 0.5;
-                if ((tick * zoomRatio * BeatUnit) % (BeatPerBar * 4) == 0)
-                {
+                if ((tick * zoomRatio * BeatUnit) % (BeatPerBar * 4) == 0) {
                     if (!first_tick) drawingContext.DrawLine(darkPen, new Point(snappedLeft, -0.5), new Point(snappedLeft, ActualHeight + 0.5));
                     int barNumber = (int)((tick * zoomRatio * BeatUnit) / BeatPerBar / 4 + 1);
 
                     FormattedText fText;
-                    if (!fTextPool.ContainsKey(barNumber))
-                    {
+                    if (!fTextPool.ContainsKey(barNumber)) {
                         fText = new FormattedText(
                             barNumber.ToString(),
                             System.Threading.Thread.CurrentThread.CurrentUICulture,
                             FlowDirection.LeftToRight, SystemFonts.CaptionFontFamily.GetTypefaces().First(),
                             12,
-                            darkPen.Brush);
+                            darkPen.Brush,
+                            VisualTreeHelper.GetDpi(this).PixelsPerDip);
                         fTextPool.Add(barNumber, fText);
-                    }
-                    else fText = fTextPool[barNumber];
+                    } else fText = fTextPool[barNumber];
                     drawingContext.DrawText(fText, new Point(snappedLeft + 3, 3));
                 }
                 left += interval;
