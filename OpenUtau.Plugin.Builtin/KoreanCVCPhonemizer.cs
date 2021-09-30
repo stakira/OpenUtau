2366fd61ee4ca84e0c1bcf08628d48fe73fceea6
baNyaNa <91547237+baNyaNa@users.noreply.github.com>
2021-09-30 20:08:58 +0900

    Update KoreanCVCPhonemizer.cs

--- OpenUtau.Plugin.Builtin/KoreanCVCPhonemizer.cs
+++ OpenUtau.Plugin.Builtin/KoreanCVCPhonemizer.cs
@@ -6,7 +6,7 @@ using OpenUtau.Core.Ustx;
 using Serilog;
 
 namespace OpenUtau.Plugin.Builtin {
-    [Phonemizer("KoreanCVCPhonemizer", "KR CVC", "NANA")]
+    [Phonemizer("KoreanCVCPhonemizer", "K CVC", "NANA")]
 
     public class KoreanCVCPhonemizer : Phonemizer {
    
