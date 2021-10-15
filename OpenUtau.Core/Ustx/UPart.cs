bf8bd8c03e138a3cb4cef85340886045e4b9ab25
Sugita Akira <akira3weet@gmail.com>
2021-10-14 18:48:45 -0700

    Experimental change log.

--- OpenUtau.Core/Ustx/UPart.cs
+++ OpenUtau.Core/Ustx/UPart.cs
@@ -206,7 +206,7 @@ namespace OpenUtau.Core.Ustx {
         }
 
         public override void AfterLoad(UProject project, UTrack track) {
-            FilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(project.FilePath), relativePath));
+            FilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(project.FilePath), relativePath ?? ""));
             Load(project);
         }
     }
