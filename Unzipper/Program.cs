a7517c5d595eefe5db652f6702d60e35f6df2f55
Sugita Akira <akira3weet@gmail.com>
2021-10-29 21:55:54 -0700

    Switch unzipper to .net framework 4.5

--- Unzipper/Program.cs
+++ Unzipper/Program.cs
@@ -8,7 +8,6 @@ namespace Unzipper {
         /// </summary>
         [STAThread]
         static void Main() {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new UnzipperForm());
