b6807e9d05e3532bc3f536b02728514367bf30d5
Sugita Akira <akira3weet@gmail.com>
2021-10-30 21:29:22 -0700

    Rename OpenUtau.App project to OpenUtau.

--- OpenUtau/Resources/Resources.Designer.cs
+++ OpenUtau/Resources/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace OpenUtau.App.Resources {
+namespace OpenUtau.Resources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace OpenUtau.App.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OpenUtau.App.Resources.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OpenUtau.Resources.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
