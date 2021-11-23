577baf56cc5475a57b622a217130f7f7d85336fb
Sugita Akira <akira3weet@gmail.com>
2021-11-22 18:15:04 -0800

    Build self-contained builds.

--- Misc/after_build.ps1
+++ Misc/after_build.ps1
@@ -1,3 +1,5 @@
 ((Get-Content -path .\Misc\release.xml -Raw) -replace '{{version}}', $env:APPVEYOR_BUILD_VERSION) | Set-Content -Path .\release.xml
 ((Get-Content -path .\Misc\appcast.xml -Raw) -replace '{{version}}', $env:APPVEYOR_BUILD_VERSION -replace '{{os}}', 'windows') | Set-Content -Path .\appcast.win.xml
+((Get-Content -path .\Misc\appcast.xml -Raw) -replace '{{version}}', $env:APPVEYOR_BUILD_VERSION -replace '{{os}}', 'windows') | Set-Content -Path .\appcast.win-x86.xml
+((Get-Content -path .\Misc\appcast.xml -Raw) -replace '{{version}}', $env:APPVEYOR_BUILD_VERSION -replace '{{os}}', 'windows') | Set-Content -Path .\appcast.win-x64.xml
 git log master -8 --no-merges --pretty=format:'%cd %s %b' --date=format:'%Y-%m-%d' > changelog.txt
