50300b49e2b2b361385653cd93e189f7b3c2cd7e
Lotte V <61566058+lottev1991@users.noreply.github.com>
2022-03-25 21:10:15 +0100

    Fix accidentally-removed code

--- OpenUtau.Plugin.Builtin/SpanishSyllableBasedPhonemizer.cs
+++ OpenUtau.Plugin.Builtin/SpanishSyllableBasedPhonemizer.cs
@@ -173,6 +173,9 @@ namespace OpenUtau.Plugin.Builtin {
                 if (i + 1 < lastC)
                 {
                     var cc2 = $"{cc[i + 1]} {cc[i + 2]}";
+                    if (!HasOto(cc2, syllable.tone)) {
+                        cc2 = $"{cc[i + 1]}{cc[i + 2]}";
+                    }
                     if (HasOto(cc1, syllable.tone) && HasOto(cc2, syllable.tone))
                     {
                         // like [V C1] [C1 C2] [C2 C3] [C3 ..]
