ddc70df2b56809b5e2bdbac553c21506f8077cec
Sugita Akira <akira3weet@gmail.com>
2022-05-05 01:25:46 -0700

    oto viewer async spectrogram and short cuts

--- OpenUtau/Views/SingersDialog.axaml.cs
+++ OpenUtau/Views/SingersDialog.axaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
@@ -12,23 +14,37 @@ using NWaves.FeatureExtractors;
 using NWaves.FeatureExtractors.Options;
 using NWaves.Filters.Fda;
 using OpenUtau.App.ViewModels;
+using ScottPlot;
 using ScottPlot.Avalonia;
+using ScottPlot.Plottable;
 using Serilog;
 
 namespace OpenUtau.App.Views {
     public partial class SingersDialog : Window {
+        const int fftSize = 1024;
+        const int melSize = 80;
+
         Color blueFill;
         Color pinkFill;
         Color blueLine;
 
+        DataGrid? otoGrid;
         AvaPlot? otoPlot;
         double coordToMs;
         double lastPointerMs;
+
+        WaveFile? wav;
+        string? wavPath;
+        IPlottable? waveform;
+        IPlottable? spectrogram;
+        List<IPlottable> timingMarks = new List<IPlottable>();
+        AxisLimits outerLimites;
 
         public SingersDialog() {
             try {
                 InitializeComponent();
 
+                otoGrid = this.Find<DataGrid>("OtoGrid");
                 otoPlot = this.Find<AvaPlot>("OtoPlot");
                 otoPlot.Configuration.LockVerticalAxis = true;
                 otoPlot.Configuration.MiddleClickDragZoom = false;
@@ -93,21 +109,98 @@ namespace OpenUtau.App.Views {
                 return;
             }
             var limites = otoPlot.Plot.GetAxisLimits();
-            otoPlot.Plot.Clear();
-            WaveFile wav;
-            using (var stream = File.OpenRead(oto.File)) {
-                wav = new WaveFile(stream);
+            bool loadWav = wavPath != oto.File;
+            if (loadWav) {
+                try {
+                    using (var stream = File.OpenRead(oto.File)) {
+                        wav = new WaveFile(stream);
+                        wavPath = oto.File;
+                    }
+                    double hopSize = GetHopSize(wav.WaveFmt.SamplingRate);
+                    outerLimites = new AxisLimits(0, wav.Signals[0].Length / hopSize, 0, 120);
+                    otoPlot.Plot.SetOuterViewLimits(
+                        outerLimites.XMin, outerLimites.XMax,
+                        outerLimites.YMin, outerLimites.YMax);
+                    otoPlot.Plot.SetAxisLimitsY(
+                        outerLimites.YMin, outerLimites.YMax);
+                } catch (Exception e) {
+                    Log.Error(e, "failed to load wav");
+                    wav = null;
+                    wavPath = null;
+                }
+            }
+            if (wav == null) {
+                otoPlot.Plot.Clear();
+                return;
             }
 
-            int fftSize = 1024;
-            int melSize = 80;
-            int hopSize = wav.WaveFmt.SamplingRate / 100;
+            if (loadWav) {
+                var samples = wav.Signals[0].Samples.Select(f => (double)f * 20 + 100).ToArray();
+                var xs = Enumerable.Range(0, samples.Length)
+                    .Select(i => i * 1.0 / GetHopSize(wav.WaveFmt.SamplingRate))
+                    .ToArray();
+                if (waveform != null) {
+                    otoPlot.Plot.Remove(waveform);
+                }
+                waveform = otoPlot.Plot.AddSignalXY(xs, samples, color: Color.Blue);
 
+                var scheduler = TaskScheduler.FromCurrentSynchronizationContext();
+                Task.Run(() => {
+                    var bands = FilterBanks.MelBands(melSize, wav.WaveFmt.SamplingRate);
+                    var extractor = new FilterbankExtractor(
+                       new FilterbankOptions {
+                           SamplingRate = wav.WaveFmt.SamplingRate,
+                           FrameSize = fftSize,
+                           FftSize = fftSize,
+                           HopSize = GetHopSize(wav.WaveFmt.SamplingRate),
+                           Window = NWaves.Windows.WindowType.Hann,
+                           FilterBank = FilterBanks.Triangular(
+                               fftSize, wav.WaveFmt.SamplingRate, bands),
+                       });
+                    var padded = new float[fftSize / 2].Concat(wav.Signals[0].Samples).Concat(new float[fftSize / 2]).ToArray();
+                    var mel = extractor.ComputeFrom(padded);
+                    var heatmap = new double[mel[0].Length, mel.Count];
+                    for (int i = 0; i < mel.Count; i++) {
+                        for (int j = 0; j < mel[i].Length; j++) {
+                            heatmap[melSize - 1 - j, i] = Math.Log(Math.Max(mel[i][j], 1e-4));
+                        }
+                    }
+                    return heatmap;
+                }).ContinueWith(heatmap => {
+                    if (spectrogram != null) {
+                        otoPlot.Plot.Remove(spectrogram);
+                        spectrogram = null;
+                    }
+                    if (heatmap.IsFaulted) {
+                        return;
+                    }
+                    spectrogram = otoPlot.Plot.AddHeatmap(heatmap.Result, lockScales: false);
+                    DrawTiming(oto);
+                    otoPlot.Refresh();
+                }, scheduler);
+            }
+            DrawTiming(oto);
+
+            if (loadWav) {
+                ZoomAll();
+            } else {
+                otoPlot.Plot.SetAxisLimits(limites);
+            }
+            otoPlot.Refresh();
+        }
+
+        void DrawTiming(Core.Ustx.UOto oto) {
+            if (otoPlot == null || wav == null) {
+                return;
+            }
+            foreach (var plottable in timingMarks) {
+                otoPlot.Plot.Remove(plottable);
+            }
+            timingMarks.Clear();
+
+            int hopSize = GetHopSize(wav.WaveFmt.SamplingRate);
             var msToCoord = 0.001 * wav.WaveFmt.SamplingRate / hopSize;
             coordToMs = 1.0 / msToCoord;
-            var dur = wav.Signals[0].Duration * 1000;
-            var samples = wav.Signals[0].Samples.Select(f => (double)f * 20 + 100).ToArray();
-            var xs = Enumerable.Range(0, samples.Length).Select(i => i * 1.0 / hopSize).ToArray();
             double cutoff = oto.Cutoff > 0
                 ? (wav.Signals[0].Duration * 1000.0 - oto.Cutoff)
                 : oto.Offset - oto.Cutoff;
@@ -116,65 +209,43 @@ namespace OpenUtau.App.Views {
             double preutterX = (oto.Offset + oto.Preutter) * msToCoord;
             double overlapX = (oto.Offset + oto.Overlap) * msToCoord;
             double cutoffX = cutoff * msToCoord;
-            double durX = dur * msToCoord;
+            double durX = outerLimites.XMax;
+
             if (offsetX > 0) {
-                otoPlot.Plot.AddPolygon(
+                timingMarks.Add(otoPlot.Plot.AddPolygon(
                     new double[] { 0, 0, offsetX, offsetX },
-                    new double[] { 0, 120, 120, 0 }, blueFill);
+                    new double[] { 80, 120, 120, 80 }, blueFill));
             }
             if (consonantX > offsetX) {
-                otoPlot.Plot.AddPolygon(
+                timingMarks.Add(otoPlot.Plot.AddPolygon(
                     new double[] { offsetX, offsetX, consonantX, consonantX },
-                    new double[] { 0, 120, 120, 0 }, pinkFill);
+                    new double[] { 80, 120, 120, 80 }, pinkFill));
             }
-            if (cutoff <= samples.Length) {
-                otoPlot.Plot.AddPolygon(
+            if (cutoff <= wav.Signals[0].Length) {
+                timingMarks.Add(otoPlot.Plot.AddPolygon(
                     new double[] { cutoffX, cutoffX, durX, durX },
-                    new double[] { 0, 120, 120, 0 }, blueFill);
+                    new double[] { 80, 120, 120, 80 }, blueFill));
             }
-            otoPlot.Plot.AddSignalXY(xs, samples, color: Color.Blue);
-            otoPlot.Plot.AddLine(offsetX, 80, overlapX, 119, blueLine, 2);
-            otoPlot.Plot.AddLine(overlapX, 119, cutoffX, 119, blueLine, 2);
-            otoPlot.Plot.AddLine(cutoffX, 119, cutoffX, 80, blueLine, 2);
 
-            var bands = FilterBanks.MelBands(melSize, wav.WaveFmt.SamplingRate);
-            var extractor = new FilterbankExtractor(
-               new FilterbankOptions {
-                   SamplingRate = wav.WaveFmt.SamplingRate,
-                   FrameSize = fftSize,
-                   FftSize = fftSize,
-                   HopSize = hopSize,
-                   Window = NWaves.Windows.WindowType.Hann,
-                   FilterBank = FilterBanks.Triangular(
-                       fftSize, wav.WaveFmt.SamplingRate, bands),
-               });
-            var padded = new float[fftSize / 2].Concat(wav.Signals[0].Samples).Concat(new float[fftSize / 2]).ToArray();
-            var mel = extractor.ComputeFrom(padded);
-            var heatmap = new double[mel[0].Length, mel.Count];
-            for (int i = 0; i < mel.Count; i++) {
-                for (int j = 0; j < mel[i].Length; j++) {
-                    heatmap[melSize - 1 - j, i] = Math.Log(Math.Max(mel[i][j], 1e-4));
-                }
+            timingMarks.Add(otoPlot.Plot.AddLine(offsetX, 80, overlapX, 119, blueLine, 2));
+            timingMarks.Add(otoPlot.Plot.AddLine(overlapX, 119, cutoffX, 119, blueLine, 2));
+            timingMarks.Add(otoPlot.Plot.AddLine(cutoffX, 119, cutoffX, 80, blueLine, 2));
+
+            timingMarks.Add(otoPlot.Plot.AddVerticalLine(overlapX, Color.Lime));
+            timingMarks.Add(otoPlot.Plot.AddText("OVL", overlapX, 80, color: Color.Lime));
+            timingMarks.Add(otoPlot.Plot.AddVerticalLine(preutterX, Color.Red));
+            timingMarks.Add(otoPlot.Plot.AddText("PRE", preutterX, 80, color: Color.Red));
+        }
+
+        void ZoomAll() {
+            if (otoPlot == null || wav == null) {
+                return;
             }
-            otoPlot.Plot.AddHeatmap(heatmap, lockScales: false);
+            otoPlot.Plot.SetAxisLimitsX(outerLimites.XMin, outerLimites.XMax);
+        }
 
-            otoPlot.Plot.AddVerticalLine(overlapX, Color.Lime);
-            otoPlot.Plot.AddText("OVL", overlapX, 80, color: Color.Lime);
-            otoPlot.Plot.AddVerticalLine(preutterX, Color.Red);
-            otoPlot.Plot.AddText("PRE", preutterX, 80, color: Color.Red);
-
-            if (fit) {
-                double span = (cutoffX - preutterX) * 2;
-                if (span <= 0) {
-                    span = durX;
-                }
-                otoPlot.Plot.SetAxisLimitsX(Math.Max(0, preutterX - span), Math.Min(durX, preutterX + span));
-                otoPlot.Plot.SetAxisLimitsY(0, 120);
-            } else {
-                otoPlot.Plot.SetAxisLimits(limites);
-            }
-            otoPlot.Plot.SetOuterViewLimits(0, durX, 0, 120);
-            otoPlot.Refresh();
+        int GetHopSize(int sampleRate) {
+            return sampleRate / 400;
         }
 
         void OtoPlot_OnPointerMoved(object sender, PointerEventArgs args) {
@@ -185,7 +256,7 @@ namespace OpenUtau.App.Views {
             lastPointerMs = otoPlot.Plot.GetCoordinateX((float)point.Position.X) * coordToMs;
         }
 
-        void OtoPlot_OnKeyDown(object sender, KeyEventArgs args) {
+        void OnKeyDown(object sender, KeyEventArgs args) {
             if (otoPlot == null) {
                 return;
             }
@@ -214,6 +285,60 @@ namespace OpenUtau.App.Views {
                 case Key.D5:
                     viewModel.SetCutoff(lastPointerMs);
                     DrawOto(viewModel.SelectedOto);
+                    break;
+                case Key.W: {
+                        var limites = otoPlot.Plot.GetAxisLimits();
+                        double span = limites.XSpan / 2;
+                        otoPlot.Plot.SetAxisLimitsX(
+                            limites.XCenter - span * 0.5,
+                            limites.XCenter + span * 0.5);
+                        otoPlot.Refresh();
+                        break;
+                    }
+                case Key.S: {
+                        var limites = otoPlot.Plot.GetAxisLimits();
+                        double span = limites.XSpan * 2;
+                        otoPlot.Plot.SetAxisLimitsX(
+                            limites.XCenter - span * 0.5,
+                            limites.XCenter + span * 0.5);
+                        otoPlot.Refresh();
+                        break;
+                    }
+                case Key.A: {
+                        var limites = otoPlot.Plot.GetAxisLimits();
+                        double shift = limites.XSpan * 0.5;
+                        otoPlot.Plot.SetAxisLimitsX(
+                            limites.XMin - shift,
+                            limites.XMax - shift);
+                        otoPlot.Refresh();
+                        break;
+                    }
+                case Key.D: {
+                        var limites = otoPlot.Plot.GetAxisLimits();
+                        double shift = limites.XSpan * 0.5;
+                        otoPlot.Plot.SetAxisLimitsX(
+                            limites.XMin + shift,
+                            limites.XMax + shift);
+                        otoPlot.Refresh();
+                        break;
+                    }
+                case Key.Q: {
+                        if (otoGrid != null) {
+                            otoGrid.SelectedIndex = Math.Max(0, otoGrid.SelectedIndex - 1);
+                            otoGrid.ScrollIntoView(otoGrid.SelectedItem, otoGrid.Columns[0]);
+                        }
+                        break;
+                    }
+                case Key.E: {
+                        if (otoGrid != null) {
+                            otoGrid.SelectedIndex++;
+                            otoGrid.ScrollIntoView(otoGrid.SelectedItem, otoGrid.Columns[0]);
+                        }
+                        break;
+                    }
+                case Key.F:
+                    ZoomAll();
+                    otoPlot.Refresh();
                     break;
                 default:
                     args.Handled = false;
