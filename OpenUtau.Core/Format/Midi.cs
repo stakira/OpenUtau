e493b3920758d4583e8577f5457d10fcfac461bf
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2022-07-21 15:01:55 +0800

    auto encoding detection

--- OpenUtau.Core/Format/Midi.cs
+++ OpenUtau.Core/Format/Midi.cs
@@ -5,21 +5,53 @@ using System.Text;
 using System.Threading.Tasks;
 
 using NAudio.Midi;
+using NChardet;
 
 using OpenUtau.Core.Ustx;
 using OpenUtau.Core.Util;
 
 namespace OpenUtau.Core.Format
 {
+    public class MyCharsetDetectionObserver: NChardet.ICharsetDetectionObserver
+    {
+        public string Charset = null;
+
+        public void Notify(string charset) {
+            Charset = charset;
+        }
+    }
     public static class Midi
     {
         static public List<UVoicePart> Load(string file, UProject project)
         {
+
             List<UVoicePart> resultParts = new List<UVoicePart>();
             MidiFile midi = new MidiFile(file);
             string lyric = NotePresets.Default.DefaultLyric;
             for (int i = 0; i < midi.Tracks; i++)
             {
+                //detect lyric encoding
+                Detector det = new Detector(6);
+                MyCharsetDetectionObserver cdo = new MyCharsetDetectionObserver();
+                det.Init(cdo);
+                bool done = false;
+                foreach (var e in midi.Events.GetTrackEvents(i)) {
+                    if (e is TextEvent) { //Lyric event
+                        var _e = e as TextEvent;
+                        if (_e.MetaEventType == MetaEventType.Lyric) {
+                            done = det.DoIt(_e.Data, _e.Data.Length, false);
+                            if (done) {
+                                break;
+                            }
+                        }
+                    }
+                }
+                det.DataEnd();
+                string Charset = cdo.Charset;
+                Encoding lyricEncoding = Encoding.UTF8;
+                if (Charset!=null){
+                    lyricEncoding = Encoding.GetEncoding(Charset);
+                }
                 Dictionary<int, UVoicePart> parts = new Dictionary<int, UVoicePart>();
                 foreach (var e in midi.Events.GetTrackEvents(i)) {
                     if (e is NoteOnEvent) {
@@ -43,7 +75,7 @@ namespace OpenUtau.Core.Format
                     else if (e is TextEvent) { //Lyric event
                         var _e = e as TextEvent;
                         if(_e.MetaEventType == MetaEventType.Lyric) {
-                            lyric = _e.Text;
+                            lyric = lyricEncoding.GetString(_e.Data);
                         }
                     }
                 }
