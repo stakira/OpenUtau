f25062ff9a465cb178e8811d5eed870be9ffe1a4
Sugita Akira <akira3weet@gmail.com>
2022-09-21 23:17:42 -0700

    [nobuild] Adds JA  CVVC test skeleton

---
+++ OpenUtau.Test/Plugins/JaCvvcTest.cs
@@ -0,0 +1,29 @@
+﻿using OpenUtau.Api;
+using OpenUtau.Plugin.Builtin;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace OpenUtau.Plugins {
+    public class JaCvvcTest : PhonemizerTestBase {
+        public JaCvvcTest(ITestOutputHelper output) : base(output) { }
+
+        protected override Phonemizer CreatePhonemizer() {
+            return new JapaneseCVVCPhonemizer();
+        }
+
+        [Theory]
+        [InlineData("ja_cvvc",
+            new string[] { "あ", "+", "あ", "-" },
+            new string[] { "C4", "C4", "C4", "C4" },
+            new string[] { "", "", "弱", "" },
+            new string[] { "- あ_C4", "a あ_弱C4", "-" })]
+        [InlineData("ja_cvvc",
+            new string[] { "お", "にょ", "ひょ", "みょ", "びょ", "ぴょ", "りょ" },
+            new string[] { "A3", "C4", "D4", "E4", "F4", "G3", "F3" },
+            new string[] { "", "弱", "", "", "強", "", "" },
+            new string[] { "- お_A3", "o ny_A3", "にょ_弱C4", "o hy_弱C4", "ひょ_C4", "o my_C4", "みょ_F4", "o by_F4", "びょ_強F4", "o py_強F4", "ぴょ_A3", "o ry_A3", "りょ_A3" })]
+        public void PhonemizeTest(string singerName, string[] lyrics, string[] tones, string[] colors, string[] aliases) {
+            RunPhonemizeTest(singerName, lyrics, tones, colors, aliases);
+        }
+    }
+}
---
+++ OpenUtau.Test/Plugins/PhonemizerTestBase.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using OpenUtau.Api;
+using OpenUtau.Classic;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace OpenUtau.Plugins {
+    public abstract class PhonemizerTestBase {
+        protected readonly ITestOutputHelper output;
+
+        protected abstract Phonemizer CreatePhonemizer();
+
+        public PhonemizerTestBase(ITestOutputHelper output) {
+            this.output = output;
+        }
+
+        public void RunPhonemizeTest(string singerName, string[] lyrics, string[] tones, string[] colors, string[] aliases) {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var basePath = Path.Join(dir, "Files");
+            var file = Path.Join(basePath, singerName, "character.txt");
+
+            var voicebank = new Voicebank() { File = file, BasePath = dir };
+            VoicebankLoader.LoadVoicebank(voicebank);
+            var singer = new ClassicSinger(voicebank);
+            singer.EnsureLoaded();
+
+            var timeAxis = new Core.TimeAxis();
+            timeAxis.BuildSegments(new Core.Ustx.UProject());
+
+            var phonemizer = CreatePhonemizer();
+            phonemizer.SetSinger(singer);
+            phonemizer.SetTiming(timeAxis);
+
+            var results = new List<Phonemizer.Result>();
+            var groups = GetNotes(lyrics, tones, colors);
+            for (var i = 0; i < groups.Count; i++) {
+                results.Add(phonemizer.Process(
+                    groups[i],
+                    i > 0 ? groups[i - 1].First() : null,
+                    i < groups.Count - 1 ? groups[i + 1].First() : null,
+                    i > 0 ? groups[i - 1].First() : null,
+                    i < groups.Count - 1 ? groups[i + 1].First() : null,
+                    i > 0 ? groups[i - 1] : null));
+            }
+            var resultAliases = results.SelectMany(r => r.phonemes).Select(p => p.phoneme).ToArray();
+
+            var builder = new StringBuilder();
+            foreach (var resultAlias in resultAliases) {
+                builder.Append("\"");
+                builder.Append(resultAlias);
+                builder.Append("\", ");
+            }
+            output.WriteLine(builder.ToString());
+
+            Assert.Equal(aliases, resultAliases);
+        }
+
+        List<Phonemizer.Note[]> GetNotes(string[] lyrics, string[] tones, string[] colors) {
+            Assert.Equal(lyrics.Length, tones.Length);
+            Assert.Equal(lyrics.Length, colors.Length);
+            var result = new List<Phonemizer.Note[]>();
+            var group = new List<Phonemizer.Note>();
+            int position = 240;
+            for (var i = 0; i < lyrics.Length; i++) {
+                var lyric = lyrics[i];
+                if (!lyric.StartsWith("+") && group.Count > 0) {
+                    result.Add(group.ToArray());
+                    group.Clear();
+                }
+                group.Add(new Phonemizer.Note {
+                    lyric = lyric,
+                    duration = 240,
+                    position = position,
+                    tone = Core.MusicMath.NameToTone(tones[i]),
+                    phonemeAttributes = new Phonemizer.PhonemeAttributes[] {
+                        new Phonemizer.PhonemeAttributes{
+                            index = 0,
+                            consonantStretchRatio = 1,
+                            voiceColor = colors[i],
+                        }
+                    },
+                });
+                position += 240;
+            }
+            if (group.Count > 0) {
+                result.Add(group.ToArray());
+                group.Clear();
+            }
+            return result;
+        }
+    }
+}
