ff7b073362766116b4cbe7934996dff28e4a3811
RGBoy/Mim <laura.nadal.fr@gmail.com>
2022-10-19 21:47:01 +0200

    Added missing vowel symbols for custom dictionary support (&, x)

--- OpenUtau.Plugin.Builtin/EnglishVCCVPhonemizer.cs
+++ OpenUtau.Plugin.Builtin/EnglishVCCVPhonemizer.cs
@@ -14,7 +14,7 @@ namespace OpenUtau.Plugin.Builtin {
     // Thanks to cubialpha, Cz and nago for their help.
     public class EnglishVCCVPhonemizer : SyllableBasedPhonemizer {
 
-        private readonly string[] vowels = "a,@,u,0,8,I,e,3,A,i,E,O,Q,6,o,1ng,9".Split(",");
+        private readonly string[] vowels = "a,@,u,0,8,I,e,3,A,i,E,O,Q,6,o,1ng,9,&,x".Split(",");
         private readonly string[] consonants = "b,ch,d,dh,f,g,h,j,k,l,m,n,ng,p,r,s,sh,t,th,v,w,y,z,zh,dd,hh,sp,st".Split(",");
         private readonly Dictionary<string, string> dictionaryReplacements = ("aa=a;ae=@;ah=u;ao=9;aw=8;ay=I;" +
             "b=b;ch=ch;d=d;dh=dh;eh=e;er=3;ey=A;f=f;g=g;hh=h;ih=i;iy=E;jh=j;k=k;l=l;m=m;n=n;ng=ng;ow=O;oy=Q;" +
