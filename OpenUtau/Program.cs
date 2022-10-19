1f837c3dcfef8f2dffc326195f3b2ac4c516d7b0
mango-ki <tamago-iku@outlook.com>
2022-10-19 20:38:17 +1100

    attempt to add file association to OpenUTAU

--- OpenUtau/Program.cs
+++ OpenUtau/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
+using Microsoft.Win32;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.ReactiveUI;
@@ -19,22 +20,58 @@ namespace OpenUtau.App {
         // yet and stuff might break.
         [STAThread]
         public static void Main(string[] args) {
+            // file association WIP
+            // if OS is windows
+            if (OS.IsWindows()) {
+                //if file extension doesn't already exist, create it
+                // (this requires administrator access at the moment)
+                if (!FileAssociation.IsAssociated(".ustx"))
+                    //associate file extension as ".ustx"
+                    //associate icon, point icon to this app's icon resource
+                    FileAssociation.Associate(".ustx", "ClassID.ProgID", "OpenUTAU sequence", "ustx.ico", System.AppDomain.CurrentDomain.BaseDirectory+"\\OpenUTAU.exe");
+            }
+
+            //else if OS is macOS
+            if (OS.IsMacOS()) {
+                //if file extension doesn't already exist, create it
+
+                //associate file extension as ".ustx"
+                //associate icon, point icon to this app's icon resource
+            }
+
+            //else if OS is linux/ubuntu
+            if (OS.IsLinux()) {
+                //if file extension doesn't already exist, create it
+
+                //associate file extension as ".ustx"
+                //associate icon, point icon to this app's icon resource
+            }
+
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            //start logging
             InitLogging();
+
+            //get current process name
             string processName = Process.GetCurrentProcess().ProcessName;
             if (processName != "dotnet") {
                 var exists = Process.GetProcessesByName(processName).Count() > 1;
+                //if process already exists (one process only)
                 if (exists) {
                     Log.Information($"Process {processName} already open. Exiting.");
                     return;
                 }
             }
+
+            //logging stuff
             Log.Information($"{Environment.OSVersion}");
             Log.Information($"{RuntimeInformation.OSDescription} " +
                 $"{RuntimeInformation.OSArchitecture} " +
                 $"{RuntimeInformation.ProcessArchitecture}");
             Log.Information($"OpenUtau v{Assembly.GetEntryAssembly()?.GetName().Version} " +
                 $"{RuntimeInformation.RuntimeIdentifier}");
+
+            //try running the app
             try {
                 Run(args);
                 Log.Information($"Exiting.");
@@ -74,6 +111,41 @@ namespace OpenUtau.App {
                 Log.Error((Exception)args.ExceptionObject, "Unhandled exception");
             });
             Log.Information("Logging initialized.");
+        }
+    }
+
+    //set file association
+    public class FileAssociation {
+        // Associate file extension with progID, description, icon and application
+        public static void Associate(string extension, string progID, string description, string icon, string application) {
+            Registry.ClassesRoot.CreateSubKey(extension).SetValue("", progID);
+            if (progID != null && progID.Length > 0)
+                using (RegistryKey key = Registry.ClassesRoot.CreateSubKey(progID)) {
+                    if (description != null)
+                        key.SetValue("", description);
+                    if (icon != null)
+                        key.CreateSubKey("DefaultIcon").SetValue("", ToShortPathName(icon));
+                    if (application != null)
+                        key.CreateSubKey(@"Shell\Open\Command").SetValue("",
+                                    ToShortPathName(application) + " \"%1\"");
+                }
+        }
+
+        // Return true if extension already associated in registry
+        public static bool IsAssociated(string extension) {
+            return (Registry.ClassesRoot.OpenSubKey(extension, false) != null);
+        }
+
+        [DllImport("Kernel32.dll")]
+        private static extern uint GetShortPathName(string lpszLongPath,
+            [Out] StringBuilder lpszShortPath, uint cchBuffer);
+
+        // Return short path format of a file name
+        private static string ToShortPathName(string longName) {
+            StringBuilder s = new StringBuilder(1000);
+            uint iSize = (uint)s.Capacity;
+            uint iRet = GetShortPathName(longName, s, iSize);
+            return s.ToString();
         }
     }
 }
