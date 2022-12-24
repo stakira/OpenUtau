358900f00dbd50306271162451edc20815e5547b
Astel123457 <41224955+Astel123457@users.noreply.github.com>
2022-12-24 11:24:41 -0500

    Change the VCV phonemizer to use wanakana instead
    This might need to be refactored eventually, because i am not such a great coder, but, it works.

--- OpenUtau.Plugin.Builtin/JapaneseVCVPhonemizer.cs
+++ OpenUtau.Plugin.Builtin/JapaneseVCVPhonemizer.cs
@@ -1,36 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using OpenUtau.Api;
 using OpenUtau.Core.Ustx;
+using WanaKanaNet;
+using static OpenUtau.Api.Phonemizer;
 
 namespace OpenUtau.Plugin.Builtin {
     [Phonemizer("Japanese VCV Phonemizer", "JA VCV", language: "JA")]
     public class JapaneseVCVPhonemizer : Phonemizer {
-        /// <summary>
-        /// The lookup table to convert a hiragana to its tail vowel.
-        /// </summary>
-        static readonly string[] vowels = new string[] {
-            "a=ぁ,あ,か,が,さ,ざ,た,だ,な,は,ば,ぱ,ま,ゃ,や,ら,わ,ァ,ア,カ,ガ,サ,ザ,タ,ダ,ナ,ハ,バ,パ,マ,ャ,ヤ,ラ,ワ,a",
-            "e=ぇ,え,け,げ,せ,ぜ,て,で,ね,へ,べ,ぺ,め,れ,ゑ,ェ,エ,ケ,ゲ,セ,ゼ,テ,デ,ネ,ヘ,ベ,ペ,メ,レ,ヱ,e",
-            "i=ぃ,い,き,ぎ,し,じ,ち,ぢ,に,ひ,び,ぴ,み,り,ゐ,ィ,イ,キ,ギ,シ,ジ,チ,ヂ,ニ,ヒ,ビ,ピ,ミ,リ,ヰ,i",
-            "o=ぉ,お,こ,ご,そ,ぞ,と,ど,の,ほ,ぼ,ぽ,も,ょ,よ,ろ,を,ォ,オ,コ,ゴ,ソ,ゾ,ト,ド,ノ,ホ,ボ,ポ,モ,ョ,ヨ,ロ,ヲ,o",
-            "n=ん,n",
-            "u=ぅ,う,く,ぐ,す,ず,つ,づ,ぬ,ふ,ぶ,ぷ,む,ゅ,ゆ,る,ゥ,ウ,ク,グ,ス,ズ,ツ,ヅ,ヌ,フ,ブ,プ,ム,ュ,ユ,ル,ヴ,u",
-            "N=ン,ng",
-        };
-
-        static readonly Dictionary<string, string> vowelLookup;
-
-        static JapaneseVCVPhonemizer() {
-            // Converts the lookup table from raw strings to a dictionary for better performance.
-            vowelLookup = vowels.ToList()
-                .SelectMany(line => {
-                    var parts = line.Split('=');
-                    return parts[1].Split(',').Select(cv => (cv, parts[0]));
-                })
-                .ToDictionary(t => t.Item1, t => t.Item2);
-        }
-
+     
         private USinger singer;
 
         // Simply stores the singer in a field.
@@ -50,16 +29,16 @@ namespace OpenUtau.Plugin.Builtin {
             }
             // The alias for no previous neighbour note. For example, "- な" for "な".
             var phoneme = $"- {note.lyric}";
-            if (prevNeighbour != null) {
+            if (prevNeighbour != null) { 
                 // If there is a previous neighbour note, first get its hint or lyric.
                 var lyric = prevNeighbour?.phoneticHint ?? prevNeighbour?.lyric;
-                // Get the last unicode element of the hint or lyric. For example, "ゃ" from "きゃ" or "- きゃ".
-                var unicode = ToUnicodeElements(lyric);
-                // Look up the trailing vowel. For example "a" for "ゃ".
-                if (vowelLookup.TryGetValue(unicode.LastOrDefault() ?? string.Empty, out var vow)) {
-                    // Now replace "- な" initially set to "a な".
-                    phoneme = $"{vow} {note.lyric}";
-                }
+                // Get the trailing vowel from wanakana
+                var vowel = WanaKana.ToRomaji(lyric);
+                // Get the last symbol in the string
+                char vow = vowel[vowel.Length- 1];
+                // Now replace "- な" initially set to "a な".
+                phoneme = $"{vow} {note.lyric}";
+                
             }
             // Get color
             string color = string.Empty;
