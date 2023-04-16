51aa33ca3908976dcdc82634eb56c98f674fa10d
Mark Beltran <markmbeltran@gmail.com>
2023-04-16 20:22:31 +0800

    Add tests for a few SBPs

---
+++ OpenUtau.Test/Plugins/EnTetoTest.cs
@@ -0,0 +1,23 @@
+﻿using OpenUtau.Api;
+using OpenUtau.Plugin.Builtin;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace OpenUtau.Plugins {
+    public class EnTetoTest : PhonemizerTestBase {
+        public EnTetoTest(ITestOutputHelper output) : base(output) { }
+        protected override Phonemizer CreatePhonemizer() {
+            return new TetoEnglishPhonemizer();
+        }
+
+        [Theory]
+        [InlineData("en_teto",
+            new string[] { "test", "words" },
+            new string[] { "C4", "C4" },
+            new string[] { "", "", },
+            new string[] { "- tE", "E s", "st", "tw", "w3", "3 d", "d z-" })]
+        public void BasicPhonemizingTest(string singerName, string[] lyrics, string[] tones, string[] colors, string[] aliases) {
+            RunPhonemizeTest(singerName, lyrics, tones, colors, aliases);
+        }
+    }
+}
