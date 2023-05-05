0239fd2a6093cb91aab9e94afe799804d8adece8
Maiko <maiko3tattun@gmail.com>
2023-05-05 18:58:36 +0900

    Add lyrics replace

--- OpenUtau/ViewModels/LyricsReplaceViewModel.cs
+++ OpenUtau/ViewModels/LyricsReplaceViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using OpenUtau.Core.Util;
+using DynamicData.Binding;
+using OpenUtau.Core;
+using OpenUtau.Core.Ustx;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 
@@ -9,50 +11,72 @@ namespace OpenUtau.App.ViewModels {
     class LyricsReplaceViewModel : ViewModelBase {
         [Reactive] public string OldValue { get; set; } = "";
         [Reactive] public string NewValue { get; set; } = "";
-        [Reactive] public string Preview { get; private set; } = "";
+        [Reactive] public string Preview { get; set; } = "";
         public List<ReplacePreset> PresetList { get; } = new List<ReplacePreset>() { //Increase!
+            new ReplacePreset(ThemeManager.GetString("-"), "", ""),
             new ReplacePreset(ThemeManager.GetString("lyricsreplace.preset.rmvalphabet"), @"[a-zA-Z]", ""),
             new ReplacePreset(ThemeManager.GetString("lyricsreplace.preset.rmvnonhiragana"), @"[^\p{IsHiragana}ヴ]+", ""),
             new ReplacePreset(ThemeManager.GetString("lyricsreplace.preset.rmvphonetichint"), @"\[.*\]", ""),
             new ReplacePreset(ThemeManager.GetString("lyricsreplace.preset.rmvtone"), @"_?[A-G](#|b)?[1-7]", ""),
             new ReplacePreset(ThemeManager.GetString("lyricsreplace.preset.rmvspace"), ".* ", "")
         };
-        [Reactive] public ReplacePreset? SelectedPreset { get; set; }
+        [Reactive] public ReplacePreset SelectedPreset { get; set; } = new ReplacePreset();
         public string[] Lyrics { get; private set; }
 
-        private string startLyrics;
+        private UVoicePart part;
+        private UNote[] notes;
+        private string[] startLyrics;
 
-        public LyricsReplaceViewModel(string[] lyrics) {
-            startLyrics = SplitLyrics.Join(lyrics);
-            Preview = SplitLyrics.Join(lyrics);
+        public LyricsReplaceViewModel(UVoicePart part, UNote[] notes, string[] lyrics) {
+            this.part = part;
+            this.notes = notes;
+            startLyrics = lyrics.Clone() as string[];
+            Preview = string.Join(", ", lyrics);
             Lyrics = lyrics;
+
             this.WhenAnyValue(x => x.OldValue, x => x.NewValue)
                 .Subscribe(t =>
                 {
-                    Preview = Regex.Replace(startLyrics, t.Item1, t.Item2);
+                    Preview = Replace();
                 });
-            this.WhenAnyValue(x => SelectedPreset)
-                .Subscribe(t => {
-                    if (t != null) {
-                        OldValue = t.OldValue;
-                        NewValue = t.NewValue;
+            this.WhenValueChanged(x => SelectedPreset)
+                .Subscribe(p => {
+                    if (SelectedPreset != null) {
+                        OldValue = SelectedPreset.OldValue;
+                        NewValue = SelectedPreset.NewValue;
                     }
                 });
         }
 
-        public void Cancel() {
+        public string Replace() {
 
+            for (int i = 0; i < startLyrics.Length; i++) {
+                Lyrics[i] = Regex.Replace(startLyrics[i], OldValue, NewValue);
+            }
+            return string.Join(", ", Lyrics);
         }
 
         public void Finish() {
-            Lyrics = SplitLyrics.Split(Preview).ToArray();
+            DocManager.Inst.StartUndoGroup();
+
+            for (int i = 0; i < Lyrics.Length && i < notes.Length; ++i) {
+                if (notes[i].lyric != Lyrics[i]) {
+                    DocManager.Inst.ExecuteCmd(new ChangeNoteLyricCommand(part, notes[i], Lyrics[i]));
+                }
+            }
+
+            DocManager.Inst.EndUndoGroup();
         }
     }
 
-    class ReplacePreset {
-        public string Name { get; private set; }
-        public string OldValue { get; private set; }
-        public string NewValue { get; private set; }
+    public class ReplacePreset {
+        public string Name { get; private set; } = "";
+        public string OldValue { get; private set; } = "";
+        public string NewValue { get; private set; } = "";
+
+        public ReplacePreset() {
+
+        }
 
         public ReplacePreset(string name, string oldValue, string newValue) {
             Name = name;
