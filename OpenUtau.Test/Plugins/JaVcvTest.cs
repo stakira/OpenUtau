4518025b9839cf2d4a4a32d11edf3681c93051b2
Lotte V <61566058+lottev1991@users.noreply.github.com>
2023-05-16 20:15:41 +0200

    Add alternate strings

--- OpenUtau.Test/Plugins/JaVcvTest.cs
+++ OpenUtau.Test/Plugins/JaVcvTest.cs
@@ -1,4 +1,4 @@
-﻿using OpenUtau.Api;
+using OpenUtau.Api;
 using OpenUtau.Plugin.Builtin;
 using Xunit;
 using Xunit.Abstractions;
@@ -14,21 +14,24 @@ namespace OpenUtau.Plugins {
         [Theory]
         [InlineData("ja_vcv",
             new string[] { "あ", "+", "あ", "-" },
+            new string[] { "", "", "", "" },
             new string[] { "C4", "C4", "C4", "C4" },
             new string[] { "", "", "Clear", "" },
             new string[] { "- あA3", "a あCA3", "-" })]
         [InlineData("ja_vcv",
             new string[] { "お", "にょ", "ひょ", "みょ", "びょ", "ぴょ", "りょ" },
+            new string[] { "", "", "", "", "", "", "" },
             new string[] { "A3", "C4", "D4", "E4", "F4", "G3", "F3" },
             new string[] { "", "Clear", "", "", "Whisper", "", "" },
             new string[] { "- おA3", "o にょCA3", "o ひょD4", "o みょD4", "o びょWD4", "o ぴょA3", "o りょA3" })]
         [InlineData("ja_vcv",
             new string[] { "- ず", "u と", "o R" },
+            new string[] { "", "", "" },
             new string[] { "A3", "C4", "D4" },
             new string[] { "", "", "" },
             new string[] { "- ずA3", "u とA3", "o RD4" })]
-        public void PhonemizeTest(string singerName, string[] lyrics, string[] tones, string[] colors, string[] aliases) {
-            RunPhonemizeTest(singerName, lyrics, tones, colors, aliases);
+        public void PhonemizeTest(string singerName, string[] lyrics, string[] alts, string[] tones, string[] colors, string[] aliases) {
+            RunPhonemizeTest(singerName, lyrics, alts, tones, colors, aliases);
         }
     }
 }
