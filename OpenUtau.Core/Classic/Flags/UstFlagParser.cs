94b00a519e2c659d47c258ff7f9ad0e065d66f73
Sugita Akira <akira3weet@gmail.com>
2023-05-21 20:01:22 -0700

    fix ust flag parsing

--- OpenUtau.Core/Classic/Flags/UstFlagParser.cs
+++ OpenUtau.Core/Classic/Flags/UstFlagParser.cs
@@ -1,63 +1,48 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace OpenUtau.Classic.Flags {
     public class UstFlagParser {
         public IList<UstFlag> Parse(string text) {
             List<UstFlag> flags = new List<UstFlag>();
-
-            int currentIndex = 0;
-            while (currentIndex < text.Length) {
-                string key = ExtractKey(text, ref currentIndex);
-                int value = ExtractValue(text, ref currentIndex);
-                flags.Add(new UstFlag(key, value));
+            var keyBuilder = new StringBuilder();
+            var valueBuilder = new StringBuilder();
+            bool wasDigit = false;
+            for (int i = 0; i <= text.Length; ++i) {
+                if (i == text.Length || char.IsLetter(text[i]) && wasDigit) {
+                    string key = keyBuilder.ToString();
+                    if (!int.TryParse(valueBuilder.ToString(), out int value)) {
+                        value = 0;
+                    }
+                    if (!string.IsNullOrEmpty(key)) {
+                        flags.Add(new UstFlag(key, value));
+                    }
+                    keyBuilder.Clear();
+                    valueBuilder.Clear();
+                }
+                if (i == text.Length) {
+                    break;
+                }
+                char c = text[i];
+                if (c == '-' || c == '+' || char.IsDigit(c)) {
+                    valueBuilder.Append(c);
+                    wasDigit = true;
+                } else if (char.IsLetter(c)) {
+                    if (keyBuilder.Length == 0 && IsSingleCharacterFlag(c)) {
+                        flags.Add(new UstFlag(c.ToString(), 0));
+                        continue;
+                    }
+                    keyBuilder.Append(c);
+                    wasDigit = false;
+                }
             }
             return flags;
         }
 
-        private string ExtractKey(string text, ref int currentIndex) {
-            string key = string.Empty;
-
-            if (IsSingleCharacterFlag(text[currentIndex])) {
-                key += text[currentIndex];
-                currentIndex++;
-                return key;
-            }
-            while (currentIndex < text.Length && Char.IsLetter(text[currentIndex])) {
-                key += text[currentIndex];
-                currentIndex++;
-            }
-            return key;
-        }
-
-        private int ExtractValue(string text, ref int currentIndex) {
-            if (currentIndex >= text.Length) {
-                return 0;
-            }
-            int value = 0;
-            bool negative = IsNegative(text[currentIndex], ref currentIndex);
-
-            while (currentIndex < text.Length && Char.IsDigit(text[currentIndex])) {
-                value = (value * 10) + (text[currentIndex] - '0');
-                currentIndex++;
-            }
-            return negative ? -value : value;
-        }
-
-        private bool IsNegative(char input, ref int currentIndex) {
-            if (input == '-') {
-                currentIndex++;
-                return true;
-            }
-            if (input == '+') {
-                currentIndex++;
-            }
-            return false;
-        }
-
         private bool IsSingleCharacterFlag(char input) {
-            var patterns = new HashSet<char> { 'N' };
+            var patterns = new HashSet<char> { 'N', 'e', 'u' };
             return patterns.Contains(input);
         }
     }
