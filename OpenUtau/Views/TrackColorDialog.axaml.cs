2c2bbea0c6dcff9aca2c25bdca749d7c1306a081
Maiko <maiko3tattun@gmail.com>
2023-06-25 18:56:39 +0900

    wip track color

---
+++ OpenUtau/Views/TrackColorDialog.axaml.cs
@@ -0,0 +1,10 @@
+﻿using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace OpenUtau.App.Views {
+    public partial class TrackColorDialog : Window {
+        public TrackColorDialog() {
+            InitializeComponent();
+        }
+    }
+}
