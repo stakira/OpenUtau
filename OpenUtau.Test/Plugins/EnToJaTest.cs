710d155186972fff4e6c383442a5b2fca82623f3
Mark Beltran <markmbeltran@gmail.com>
2023-07-12 16:55:34 +0800

    Add VCV and CV to existing tests

--- OpenUtau.Test/Plugins/EnToJaTest.cs
+++ OpenUtau.Test/Plugins/EnToJaTest.cs
@@ -1,4 +1,4 @@
-using OpenUtau.Api;
+﻿using OpenUtau.Api;
 using OpenUtau.Plugin.Builtin;
 using Xunit;
 using Xunit.Abstractions;
@@ -17,6 +17,18 @@ namespace OpenUtau.Plugins {
             new string[] { "C3", "C3" },
             new string[] { "", "", },
             new string[] { "て_A3", "e s_A3", "と_A3", "うぉ_A3", "o d_A3", "ず_A3" })]
+        [InlineData("ja_vcv",
+            new string[] { "test", "words" },
+            new string[] { "", "", },
+            new string[] { "C3", "C3" },
+            new string[] { "", "", },
+            new string[] { "- てA3", "e すA3", "u とA3", "o うぉA3", "o どA3", "o ずA3" })]
+        [InlineData("ja_cv",
+            new string[] { "test", "words" },
+            new string[] { "", "", },
+            new string[] { "C3", "C3" },
+            new string[] { "", "", },
+            new string[] { "て", "す", "と", "を", "ど", "ず" })]
         public void BasicPhonemizingTest(string singerName, string[] lyrics, string[] alts, string[] tones, string[] colors, string[] aliases) {
             RunPhonemizeTest(singerName, lyrics, alts, tones, colors, aliases);
         }
@@ -28,6 +40,12 @@ namespace OpenUtau.Plugins {
             new string[] { "C3", "C4" },
             new string[] { "", "", },
             new string[] { "て_A3", "e s_A3", "と_A3", "うぉ_C4", "o d_C4", "ず_C4" })]
+        [InlineData("ja_vcv",
+            new string[] { "test", "words" },
+            new string[] { "", "", },
+            new string[] { "C4", "C5" },
+            new string[] { "", "", },
+            new string[] { "- てA3", "e すA3", "u とA3", "o うぉC5", "o どC5", "o ずC5" })]
         public void MultipitchTest(string singerName, string[] lyrics, string[] alts, string[] tones, string[] colors, string[] aliases) {
             RunPhonemizeTest(singerName, lyrics, alts, tones, colors, aliases);
         }
@@ -39,6 +57,12 @@ namespace OpenUtau.Plugins {
             new string[] { "C4", "C4" },
             new string[] { "", "強", },
             new string[] { "て_C4", "e s_強B3", "と_C4", "うぉ_C4", "o d_C4", "ず_C4" })]
+        [InlineData("ja_vcv",
+            new string[] { "test", "words" },
+            new string[] { "", "", },
+            new string[] { "C4", "C4" },
+            new string[] { "", "Clear", },
+            new string[] { "- てA3", "e すCA3", "u とA3", "o うぉA3", "o どA3", "o ずA3" })]
         // Colors are per-phoneme
         public void VoiceColorTest(string singerName, string[] lyrics, string[] alts, string[] tones, string[] colors, string[] aliases) {
             RunPhonemizeTest(singerName, lyrics, alts, tones, colors, aliases);
