5ba5dec00ebf8d3205223b4125f0b7c15e9a83aa
Lotte V <lottevmusic@outlook.com>
2023-07-25 18:16:18 +0200

    [German for OU] Add German G2P, phonemizers, tests, custom dicts
    - Add German G2P;
    - Add German diphonic phonemizer. You could create a German addon for an Arpasing bank and it will work;
    - Add German VCCV phonemizer. Beta testing highly appreciated;
    - Add custom German dicts support;
    - Add phonemizer tests (might fix them along the way if they don't work immediately).

--- OpenUtau.Plugin.Builtin/Data/Resources.Designer.cs
+++ OpenUtau.Plugin.Builtin/Data/Resources.Designer.cs
@@ -73,9 +73,29 @@ namespace OpenUtau.Plugin.Builtin.Data {
         /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
+        internal static byte[] de_vccv_template {
+            get {
+                object obj = ResourceManager.GetObject("de_vccv_template", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
         internal static byte[] envccv_template {
             get {
                 object obj = ResourceManager.GetObject("envccv_template", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] german_template {
+            get {
+                object obj = ResourceManager.GetObject("german_template", resourceCulture);
                 return ((byte[])(obj));
             }
         }
