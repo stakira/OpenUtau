e0eef86325b22afa61eded5913ec2f49d8d72d75
Lotte V <lottevmusic@outlook.com>
2023-08-25 19:34:04 +0200

    More fixes

--- OpenUtau.Plugin.Builtin/EnXSampaPhonemizer.cs
+++ OpenUtau.Plugin.Builtin/EnXSampaPhonemizer.cs
@@ -444,7 +444,7 @@ namespace OpenUtau.Plugin.Builtin {
                 }
             } else {
                 for (var i = lastC; i >= 0; i--) {
-                    var vcc = $"{v} {string.Join("", cc.TakeLast(2))}-";
+                    var vcc = $"{v} {string.Join("", cc.Take(2))}-";
                     var vcc2 = $"{v}{string.Join(" ", cc.TakeLast(2))}-";
                     var vcc3 = $"{v}{string.Join(" ", cc.Take(2))}";
                     var vcc4 = $"{v} {string.Join("", cc.Take(2))}";
@@ -456,6 +456,7 @@ namespace OpenUtau.Plugin.Builtin {
                     } else if (HasOto(vcc, ending.tone) || HasOto(ValidateAlias(vcc), ending.tone)) {
                         phonemes.Add(vcc);
                         firstC = 1;
+                        lastC = 2;
                         break;
                     } else if (HasOto(vcc2, ending.tone) || HasOto(ValidateAlias(vcc2), ending.tone)) {
                         phonemes.Add(vcc2);
