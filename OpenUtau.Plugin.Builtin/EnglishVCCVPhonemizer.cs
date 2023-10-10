d61d28f0941e6421c1eb941e263c8f8dab915438
AnAndroNerd <87346264+AnAndroNerd@users.noreply.github.com>
2023-10-10 12:05:16 -0700

    Removed [Ang]

--- OpenUtau.Plugin.Builtin/EnglishVCCVPhonemizer.cs
+++ OpenUtau.Plugin.Builtin/EnglishVCCVPhonemizer.cs
@@ -16,7 +16,7 @@ namespace OpenUtau.Plugin.Builtin {
     // Thanks to cubialpha, Cz, Halo/BagelHero, AnAndroNerd and nago for their help.
     public class EnglishVCCVPhonemizer : SyllableBasedPhonemizer {
 
-        private readonly string[] vowels = "a,@,u,0,8,I,e,3,A,i,E,O,Q,6,o,1ng,9,&,x,1,9l,8n,Ang".Split(",");
+        private readonly string[] vowels = "a,@,u,0,8,I,e,3,A,i,E,O,Q,6,o,1ng,9,&,x,1,9l,8n".Split(",");
         private readonly string[] consonants = "b,ch,d,dh,f,g,h,j,k,l,m,n,ng,p,r,s,sh,t,th,v,w,y,z,zh,dd,hh,sp,st".Split(",");
         private readonly Dictionary<string, string> dictionaryReplacements = ("aa=a;ae=@;ah=u;ao=9;aw=8;ay=I;" +
             "b=b;ch=ch;d=d;dh=dh;eh=e;er=3;ey=A;f=f;g=g;hh=h;hhy=hh;ih=i;iy=E;jh=j;k=k;l=l;m=m;n=n;ng=ng;ow=O;oy=Q;" +
@@ -48,10 +48,7 @@ namespace OpenUtau.Plugin.Builtin {
                 {"Ol","0l"},
                 //{"6 l","6l"},
                 //{"i r","Er"},
-                {"ir","Er"},
-                {"9 l","9l"},
-                {"8 n","8n"},
-                {"A ng","Ang"}
+                {"ir","Er"}
             };
 
         private readonly Dictionary<string, string> vvExceptions =
