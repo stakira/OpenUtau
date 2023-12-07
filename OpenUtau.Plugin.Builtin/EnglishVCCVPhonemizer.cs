812adc8b40e996be20747ad7c80553b79c40d903
AnAndroNerd <87346264+AnAndroNerd@users.noreply.github.com>
2023-12-07 08:06:12 -0700

    Update EnglishVCCVPhonemizer.cs

--- OpenUtau.Plugin.Builtin/EnglishVCCVPhonemizer.cs
+++ OpenUtau.Plugin.Builtin/EnglishVCCVPhonemizer.cs
@@ -144,7 +144,7 @@ namespace OpenUtau.Plugin.Builtin {
                         var vc = $"{prevV} {vvExceptions[prevV]}";
                         if (!HasOto(vc, syllable.vowelTone)) {
                             vc = $"{prevV}{vvExceptions[prevV]}";
-                            if ({prevV} == "8n,9l")
+                            if ({prevV} == "8n,9l") {
                                 vc = $"{prevV}{vvExceptions[prevV]}-";
                         }
                         phonemes.Add(vc);
