00bebb03b4073b0f19c8c7e139a52edd91d975d2
rokujyushi <rosabu376@gmail.com>
2024-02-02 02:11:13 +0900

    機能作成開始

---
+++ OpenUtau.Core/Voicevox/VoicevoxClient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NetMQ;
+using NetMQ.Sockets;
+using Newtonsoft.Json;
+using Serilog;
+
+namespace OpenUtau.Core.Voicevox {
+    class VoicevoxClient: Util.SingletonBase<VoicevoxClient> {
+        internal T SendRequest<T>(VoicevoxURL voicevoxURL) {
+            using (var client = new RequestSocket()) {
+                client.Connect("tcp://127.0.0.1:50021");
+                string request = this.RequestURL(voicevoxURL);
+                Log.Information($"VoicevoxProcess sending {request}");
+                client.SendFrame(request);
+                client.TryReceiveFrameString(TimeSpan.FromSeconds(300), out string? message);
+                Log.Information($"VoicevoxProcess received {message}");
+                return JsonConvert.DeserializeObject<T>(message ?? string.Empty)!;
+            }
+        }
+
+        public string RequestURL(VoicevoxURL voicevoxURL) {
+            StringBuilder queryStringBuilder = new StringBuilder();
+            foreach (var parameter in voicevoxURL.query) {
+                queryStringBuilder.Append($"{parameter.Key}={parameter.Value}&");
+            }
+
+            // 末尾の余分な "&" を削除
+            string queryString = "?"+queryStringBuilder.ToString().TrimEnd('&');
+
+            string str = $"{voicevoxURL.method.ToUpper()} {voicevoxURL.path}{queryString} {voicevoxURL.protocol.ToUpper()}\r\n";
+            str += $"HOST: {voicevoxURL.host}\r\n\r\n";
+            return str ;
+        }
+    }
+    public class VoicevoxURL {
+        public string method = string.Empty;
+        public string protocol = "HTTP/1.1";
+        public string host = "127.0.0.1:50021";
+        public string path = string.Empty;
+        public Dictionary<string, string> query = new Dictionary<string, string>();
+    }
+}
---
+++ OpenUtau.Core/Voicevox/VoicevoxSinger.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using OpenUtau.Classic;
+using OpenUtau.Core.Ustx;
+using Serilog;
+using static OpenUtau.Api.Phonemizer;
+
+namespace OpenUtau.Core.Voicevox {
+    public class VoicevoxSinger : USinger {
+        public override string Id => voicebank.Id;
+        public override string Name => voicebank.Name;
+        public override Dictionary<string, string> LocalizedNames => voicebank.LocalizedNames;
+        public override USingerType SingerType => voicebank.SingerType;
+        public override string BasePath => voicebank.BasePath;
+        public override string Author => voicebank.Author;
+        public override string Voice => voicebank.Voice;
+        public override string Location => Path.GetDirectoryName(voicebank.File);
+        public override string Web => voicebank.Web;
+        public override string Version => voicebank.Version;
+        public override string OtherInfo => voicebank.OtherInfo;
+        public override IList<string> Errors => errors;
+        public override string Avatar => voicebank.Image == null ? null : Path.Combine(Location, voicebank.Image);
+        public override byte[] AvatarData => avatarData;
+        public override string Portrait => voicebank.Portrait == null ? null : Path.Combine(Location, voicebank.Portrait);
+        public override float PortraitOpacity => voicebank.PortraitOpacity;
+        public override int PortraitHeight => voicebank.PortraitHeight;
+        public override string Sample => voicebank.Sample == null ? null : Path.Combine(Location, voicebank.Sample);
+        public override string DefaultPhonemizer => voicebank.DefaultPhonemizer;
+        public override Encoding TextFileEncoding => voicebank.TextFileEncoding;
+        public override IList<USubbank> Subbanks => subbanks;
+        public override IList<UOto> Otos => otos;
+
+        Voicebank voicebank;
+        VoicevoxConfig voicevoxConfig;
+        List<string> errors = new List<string>();
+        List<USubbank> subbanks = new List<USubbank>();
+        List<UOto> otos = new List<UOto>();
+        Dictionary<string, UOto> otoMap = new Dictionary<string, UOto>();
+
+        HashSet<string> phonemes = new HashSet<string>();
+        Dictionary<string, string[]> table = new Dictionary<string, string[]>();
+
+        public byte[] avatarData;
+
+        public VoicevoxSinger(Voicebank voicebank) {
+            this.voicebank = voicebank;
+            found = true;
+        }
+
+        public override void EnsureLoaded() {
+            if (Loaded) {
+                return;
+            }
+            Reload();
+        }
+
+        public override void Reload() {
+            if (!Found) {
+                return;
+            }
+            try {
+                voicebank.Reload();
+                Load();
+                loaded = true;
+            } catch (Exception e) {
+                Log.Error(e, $"Failed to load {voicebank.File}");
+            }
+        }
+
+        void Load() {
+            voicevoxConfig = VoicevoxConfig.Load(this);
+            if (!voicevoxConfig.version.Equals("1.15.0")) {
+                Log.Error("It differs from the supported version.");
+            }
+            phonemes.Clear();
+            table.Clear();
+            otos.Clear();
+            try {
+                foreach (var line in File.ReadAllLines(voicevoxConfig.dictxtPath)) {
+                    foreach (var p in line.Split(' ')) {
+                            phonemes.Add(p);
+                    }
+                }
+            } catch (Exception e) {
+                Log.Error(e, $"Failed to load dic.txt for {Name}");
+            }
+
+            subbanks.Clear();
+            foreach(Styles style in voicevoxConfig.styles) {
+                subbanks.Add(new USubbank(new Subbank() {
+                    Prefix = string.Empty,
+                    Suffix = style.name,
+                    ToneRanges = new[] { "C1-B7" },
+                }));
+            }
+
+            var dummyOtoSet = new UOtoSet(new OtoSet(), Location);
+            foreach (var phone in phonemes) {
+                foreach (var subbank in subbanks) {
+                    var uOto = UOto.OfDummy(phone);
+                    if (!otoMap.ContainsKey(uOto.Alias)) {
+                        otos.Add(uOto);
+                        otoMap.Add(uOto.Alias, uOto);
+                    } else {
+                        //Errors.Add($"oto conflict {Otos[oto.Alias].Set}/{oto.Alias} and {otoSet.Name}/{oto.Alias}");
+                    }
+                }
+            }
+
+            if (Avatar != null && File.Exists(Avatar)) {
+                try {
+                    using (var stream = new FileStream(Avatar, FileMode.Open)) {
+                        using (var memoryStream = new MemoryStream()) {
+                            stream.CopyTo(memoryStream);
+                            avatarData = memoryStream.ToArray();
+                        }
+                    }
+                } catch (Exception e) {
+                    avatarData = null;
+                    Log.Error(e, "Failed to load avatar data.");
+                }
+            } else {
+                avatarData = null;
+                Log.Error("Avatar can't be found");
+            }
+        }
+
+        public override bool TryGetOto(string phoneme, out UOto oto) {
+            var parts = phoneme.Split();
+            if (parts.All(p => phonemes.Contains(p))) {
+                oto = UOto.OfDummy(phoneme);
+                return true;
+            }
+            oto = null;
+            return false;
+        }
+
+        public override IEnumerable<UOto> GetSuggestions(string text) {
+            if (text != null) {
+                text = text.ToLowerInvariant().Replace(" ", "");
+            }
+            bool all = string.IsNullOrEmpty(text);
+            return table.Keys
+                .Where(key => all || key.Contains(text))
+                .Select(key => UOto.OfDummy(key));
+        }
+
+        public override byte[] LoadPortrait() {
+            return string.IsNullOrEmpty(Portrait)
+                ? null
+                : File.ReadAllBytes(Portrait);
+        }
+
+        public override byte[] LoadSample() {
+            return string.IsNullOrEmpty(Sample)
+                ? null
+                : File.ReadAllBytes(Sample);
+        }
+    }
+}
