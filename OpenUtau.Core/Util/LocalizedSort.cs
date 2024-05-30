417d30728a9d459b1771c60334fa1c6e09f9a67c
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2024-05-31 07:03:27 +0800

    string.empty means not translated

--- OpenUtau.Core/Util/LocalizedSort.cs
+++ OpenUtau.Core/Util/LocalizedSort.cs
@@ -19,8 +19,12 @@ namespace OpenUtau.Core.Util {
     public static class LocalizedSort {
         public static IEnumerable<T> LocalizedOrderBy<T>(this IEnumerable<T> source, Func<T, string> selector) {
             var sortingOrder = Preferences.Default.SortingOrder;
-            if(String.IsNullOrEmpty(sortingOrder)) {
+            if(sortingOrder == null) {
+                //Follow the display language
                 sortingOrder = Preferences.Default.Language;
+            } else if(sortingOrder = String.Empty){
+                //Don't translate
+                sortingOrder = CultureInfo.InvariantCulture;
             }
             var comparer = new LocalizedComparer(CultureInfo.GetCultureInfo(sortingOrder));
             return source.OrderBy(selector, comparer);
