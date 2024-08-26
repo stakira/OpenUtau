cc6b4409a1cd2cc4efc45e9645f9417880504697
yqzhishen <yangqian_1015@icloud.com>
2024-08-26 21:15:28 +0800

    Avoid repeated dictionary and model loading in DiffSingerBasePhonemizer

--- OpenUtau.Core/DiffSinger/DiffSingerBasePhonemizer.cs
+++ OpenUtau.Core/DiffSinger/DiffSingerBasePhonemizer.cs
@@ -30,14 +30,26 @@ namespace OpenUtau.Core.DiffSinger
         string defaultPause = "SP";
         protected virtual string GetDictionaryName()=>"dsdict.yaml";
 
+        private bool _singerLoaded;
+
         public override void SetSinger(USinger singer) {
+            if (_singerLoaded && singer == this.singer) return;
+            try {
+                _singerLoaded = _executeSetSinger(singer);
+            } catch {
+                _singerLoaded = false;
+                throw;
+            }
+        }
+
+        private bool _executeSetSinger(USinger singer) {
             this.singer = singer;
-            if(singer==null){
-                return;
+            if (singer == null) {
+                return false;
             }
             if(singer.Location == null){
                 Log.Error("Singer location is null");
-                return;
+                return false;
             }
             if (File.Exists(Path.Join(singer.Location, "dsdur", "dsconfig.yaml"))) {
                 rootPath = Path.Combine(singer.Location, "dsdur");
@@ -51,7 +63,7 @@ namespace OpenUtau.Core.DiffSinger
                 dsConfig = Yaml.DefaultDeserializer.Deserialize<DsConfig>(configTxt);
             } catch(Exception e) {
                 Log.Error(e, $"failed to load dsconfig from {configPath}");
-                return;
+                return false;
             }
             this.frameMs = dsConfig.frameMs();
             //Load g2p
@@ -67,7 +79,7 @@ namespace OpenUtau.Core.DiffSinger
                 linguisticModel = new InferenceSession(linguisticModelBytes);
             } catch (Exception e) {
                 Log.Error(e, $"failed to load linguistic model from {linguisticModelPath}");
-                return;
+                return false;
             }
             var durationModelPath = Path.Join(rootPath, dsConfig.dur);
             try {
@@ -76,8 +88,9 @@ namespace OpenUtau.Core.DiffSinger
                 durationModel = new InferenceSession(durationModelBytes);
             } catch (Exception e) {
                 Log.Error(e, $"failed to load duration model from {durationModelPath}");
-                return;
+                return false;
             }
+            return true;
         }
 
         protected virtual IG2p LoadG2p(string rootPath) {
