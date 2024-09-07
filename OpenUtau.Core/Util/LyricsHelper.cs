34be80cdb5b49603d433a339b26a1f302eb142d8
tyler <103609620+spicytigermeat@users.noreply.github.com>
2024-09-07 09:06:39 -0500

    Update LyricsHelper.cs to include Thai G2p

--- OpenUtau.Core/Util/LyricsHelper.cs
+++ OpenUtau.Core/Util/LyricsHelper.cs
@@ -45,6 +45,7 @@ namespace OpenUtau.Core.Util {
             typeof(RussianG2pLyricsHelper),
             typeof(SpanishG2pLyricsHelper),
             typeof(KoreanG2pLyricsHelper),
+            typeof(ThaiG2pLyricsHelper),
         };
     }
 
@@ -119,6 +120,10 @@ namespace OpenUtau.Core.Util {
 
     public class KoreanG2pLyricsHelper : G2pLyricsHelper {
         public KoreanG2pLyricsHelper() : base(new KoreanG2p()) { }
+    }
+
+    public class ThaiG2pLyricsHelper : G2pLyricsHelper {
+        public ThaiG2pLyricsHelper() : base(new ThaiG2p()) { }
     }
 }
 
