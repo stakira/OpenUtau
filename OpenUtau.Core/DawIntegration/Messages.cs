172d2c21aab4115febc3f6934b22c1c199e556f5
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-09-15 09:30:10 +0900

    Add: Add missing file

---
+++ OpenUtau.Core/DawIntegration/Messages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenUtau.Core.Ustx;
+
+namespace OpenUtau.Core.DawIntegration {
+    public class DawMessage {
+    }
+
+    public class UpdateStatusMessage : DawMessage {
+        public string ustx;
+        public List<string> mixes;
+
+        public UpdateStatusMessage(string ustx, List<string> mixes) {
+            this.ustx = ustx;
+            this.mixes = mixes;
+        }
+    }
+}
