119ea3fddbf75734e94c168d8c2e81142d24b3b4
Maiko <maikotattun@yahoo.co.jp>
2025-02-02 12:44:41 +0900

    Fixed inability to generate singer error report when there is an invalid line in oto.ini

--- OpenUtau.Core/Classic/VoicebankErrorChecker.cs
+++ OpenUtau.Core/Classic/VoicebankErrorChecker.cs
@@ -72,6 +72,9 @@ namespace OpenUtau.Classic {
             foreach (var otoSet in voicebank.OtoSets) {
                 string dir = Path.Combine(path, Path.GetDirectoryName(otoSet.File));
                 foreach (var oto in otoSet.Otos) {
+                    if (oto.FileTrace.line == string.Empty || oto.FileTrace.line.StartsWith("#Charaset:")) {
+                        continue;
+                    }
                     if (!oto.IsValid) {
                         Errors.Add(new VoicebankError() {
                             trace = oto.FileTrace,
@@ -270,7 +273,7 @@ namespace OpenUtau.Classic {
                 .Select(Path.GetFileName)
                 .ToDictionary(x => x.ToLower(), x => x);
             foreach(string fileName in correctFileNames) {
-                if(!fileNamesLowerToActual.ContainsKey(fileName.ToLower())) {
+                if (string.IsNullOrWhiteSpace(fileName) || !fileNamesLowerToActual.ContainsKey(fileName.ToLower())) {
                     continue;
                 }
                 if (fileNamesLowerToActual[fileName.ToLower()] != fileName) {
@@ -292,7 +295,7 @@ namespace OpenUtau.Classic {
         /// <param name="otoSet">otoSet to be checked</param>
         /// <returns></returns>
         bool CheckDuplicatedNameIgnoringCase(OtoSet otoSet) {
-            var wavNames = otoSet.Otos.Select(x => x.Wav).Distinct().ToList();
+            var wavNames = otoSet.Otos.Select(x => x.Wav).Where(x => string.IsNullOrWhiteSpace(x)).Distinct().ToList();
             var duplicatedGroups = wavNames.GroupBy(x => x.ToLower())
                 .Where(group => group.Count() > 1)
                 .ToList();
