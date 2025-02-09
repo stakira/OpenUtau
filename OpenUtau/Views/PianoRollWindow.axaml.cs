d86488d79a6a6b094ad5d409bc62fc4e428d27a3
Maiko <maikotattun@yahoo.co.jp>
2025-02-09 19:47:27 +0900

    Revert "remove unnecessary code"
    This reverts commit c176aef27996a5d612ad5ff38175ac6b8db16740.

--- OpenUtau/Views/PianoRollWindow.axaml.cs
+++ OpenUtau/Views/PianoRollWindow.axaml.cs
@@ -1452,6 +1452,7 @@ namespace OpenUtau.App.Views {
                 // PLAY POSITION + SELECTION
                 case Key.Home:
                     if (isNone) {
+                        playVm.MovePlayPos(notesVm.Part.position);
                         HScrollBar.Value = HScrollBar.Minimum;
                         return true;
                     }
