ebc4801b11a31e1767dec00835d352169fb55b11
keirokeer <keirokeer@outlook.com>
2025-03-04 11:50:08 +0200

    Update EnglishCpVPhonemizer.cs

--- OpenUtau.Plugin.Builtin/EnglishCpVPhonemizer.cs
+++ OpenUtau.Plugin.Builtin/EnglishCpVPhonemizer.cs
@@ -10,7 +10,7 @@ using OpenUtau.Core.Ustx;
 using Serilog;
 
 namespace OpenUtau.Plugin.Builtin {
-    [Phonemizer("Arpabet C+V Phonemizer", "EN C+V", "Cadlaxa", language: "EN")]
+    [Phonemizer("Arpabet C+V Phonemizer", "EN C+V", "Cadlaxa", language: "UTAU")]
     // Custom C+V Phonemizer for OU
     // Arpabet only but in the future update, it can be customize the phoneme set
     public class EnglishCpVPhonemizer : SyllableBasedPhonemizer {
