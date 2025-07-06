0df21ecb700c2ade946282842a47048fad534d9f
Maiko <maikotattun@yahoo.co.jp>
2025-07-06 23:30:24 +0900

    Translate Exe Setup Dialog

--- OpenUtau/ViewModels/ExeSetupViewModel.cs
+++ OpenUtau/ViewModels/ExeSetupViewModel.cs
@@ -6,17 +6,11 @@ namespace OpenUtau.App.ViewModels {
         [Reactive] public string message { get; set; }
         public ExeSetupViewModel(string filePath) {
             this.filePath = filePath;
-            message = $"Installing {filePath}...\n\n";
+            message = string.Format(ThemeManager.GetString("exesetup.installing"), filePath);
             if (OS.IsMacOS()) {
-                message += "To use exe resamplers or wavtools on MacOS:\n"
-                    + "1. Install wine using following commands:\n"
-                    + "      % brew tap gcenx/wine\n"
-                    + "      % brew install --cask --no-quarantine wine-crossover\n"
-                    + "2. Set wine path in Preferences > Advanced > Wine Path";
+                message += "\n\n" + ThemeManager.GetString("exesetup.mac");
             } else if(OS.IsLinux()) {
-                message += "To use exe resamplers or wavtools on Linux:\n"
-                    + "1. Install wine from https://www.winehq.org/\n" 
-                    + "2. Set wine path in Preferences > Advanced > Wine Path";
+                message += "\n\n" + string.Format(ThemeManager.GetString("exesetup.linux"), "https://www.winehq.org/");
             }
         }
     }
