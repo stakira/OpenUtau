689c4bec1428f1da156126e3ed856f24f28727b5
S'pose <imsupposedto@gmail.com>
2025-08-22 21:15:12 -0700

    Add Millefeuille and Marzipan Phonemizers
    Adding Millfeuille (French) and Marzipan (German) phonemizers and lyrics helpers

--- OpenUtau.Core/Util/LyricsHelper.cs
+++ OpenUtau.Core/Util/LyricsHelper.cs
@@ -39,7 +39,9 @@ namespace OpenUtau.Core.Util {
             typeof(ArpabetG2pLyricsHelper),
             typeof(ArpabetPlusG2pLyricsHelper),
             typeof(FrenchG2pLyricsHelper),
+            typeof(FrenchMillefeuilleG2pLyricsHelper),
             typeof(GermanG2pLyricsHelper),
+            typeof(GermanMarzipanG2pLyricsHelper),
             typeof(ItalianG2pLyricsHelper),
             typeof(PortugueseG2pLyricsHelper),
             typeof(RussianG2pLyricsHelper),
@@ -94,9 +96,15 @@ namespace OpenUtau.Core.Util {
     public class FrenchG2pLyricsHelper : G2pLyricsHelper {
         public FrenchG2pLyricsHelper() : base(new FrenchG2p()) { }
     }
+    public class FrenchMillefeuilleG2pLyricsHelper : G2pLyricsHelper {
+        public FrenchMillefeuilleG2pLyricsHelper() : base(new FrenchMillefeuilleG2p()) { }
+    }
 
     public class GermanG2pLyricsHelper : G2pLyricsHelper {
         public GermanG2pLyricsHelper() : base(new GermanG2p()) { }
+    }
+    public class GermanMarzipanG2pLyricsHelper : G2pLyricsHelper {
+        public GermanMarzipanG2pLyricsHelper() : base(new GermanMarzipanG2p()) { }
     }
 
     public class ItalianG2pLyricsHelper : G2pLyricsHelper {
