f6b093692468a6e6dfd2d61d263e15c952b615a3
S'pose <imsupposedto@gmail.com>
2025-08-29 10:49:05 -0700

    Update FrenchMillefeuilleG2p.cs
    add missing character

--- OpenUtau.Core/G2p/FrenchMillefeuilleG2p.cs
+++ OpenUtau.Core/G2p/FrenchMillefeuilleG2p.cs
@@ -7,11 +7,12 @@ using OpenUtau.Api;
 namespace OpenUtau.Core.G2p {
     public class FrenchMillefeuilleG2p : G2pPack {
         private static readonly string[] graphemes = new string[] {
-            "", "", "", "", "''", "-", "a", "b", "c", "d", "e",
-            "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-            "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","è", "ê",
-            "à", "â", "î", "ô", "ù", "û", "ç", "œ", "ï", "(", ")", "0",
-            "1", "2", "3", "4", "5", "6", "7", "8", "9",
+            "", "", "", "", "'", "-", "a", "b", "c", "d", "e", "f",
+            "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
+            "r", "s", "t", "u", "v", "w", "x", "y", "z", "é",
+            "è", "ê", "à", "â", "î", "ô", "ù", "û", "ç", "œ",
+            "ï", "(", ")", "0", "1", "2", "3", "4", "5", "6",
+            "7", "8", "9",
         };
 
         private static readonly string[] phonemes = new string[] {
