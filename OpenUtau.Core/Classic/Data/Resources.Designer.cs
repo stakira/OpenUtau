70b916813c1d5be86fd66f059a18a0f13789665e
StAkira <akira3weet@gmail.com>
2025-11-27 23:24:09 -0800

    fix

--- OpenUtau.Core/Classic/Data/Resources.Designer.cs
+++ OpenUtau.Core/Classic/Data/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace OpenUtau.Core.Classic.Data {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Classic.Data.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OpenUtau.Core.Classic.Data.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
