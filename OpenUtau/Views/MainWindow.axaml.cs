6b3ec4655410b21e4cbbca4ef2da7ae368993683
atouu <67765922+atouu@users.noreply.github.com>
2026-01-24 09:59:18 +0800

    Add a menu to hide piano roll

--- OpenUtau/Views/MainWindow.axaml.cs
+++ OpenUtau/Views/MainWindow.axaml.cs
@@ -1182,6 +1182,8 @@ namespace OpenUtau.App.Views {
                 // Workaround for new window losing focus.
                 if (pianoRollWindow != null) {
                     openPianoRollWindow = true;
+                } else {
+                    viewModel.ShowPianoRoll = true;
                 }
                 int tick = viewModel.TracksViewModel.PointToTick(args.GetPosition(canvas));
                 DocManager.Inst.ExecuteCmd(new LoadPartNotification(partControl.part, DocManager.Inst.Project, tick));
